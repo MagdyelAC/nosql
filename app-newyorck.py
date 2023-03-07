17e83f4c2600aee2cfff47597fc9b9908dd50c6c
MagdyelAC <MagdyelAguilarCid@gmail.com>
2023-03-07 10:29:58 -0600

    update version 1.5

---
+++ app-newyorck.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+st.title('Carreras en bici - NYC')
+
+DATE_COLUMN = 'started_at'
+DATA_URL = ('citibike-tripdata.csv')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Cargando datos...')
+data = load_data(1000)
+
+st.sidebar.image("img.jpeg")
+st.sidebar.write("Magdyel Aguilar Cid - S20020309")
+st.sidebar.write("zS20020309@estudiantes.uv.mx")
+st.sidebar.write(dt.datetime.now())
+st.sidebar.markdown("##")
+
+if st.sidebar.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+if st.sidebar.checkbox('Recorridos por hora'):
+    st.subheader('Numero de recorridos por hora')
+
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
+
+
+# Some number in the range 0-23
+hour_to_filter = st.sidebar.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Mapa de los recorridos a las: %s:00' % hour_to_filter)
+st.map(filtered_data)
+
